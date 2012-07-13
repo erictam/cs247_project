703b4b4e32f944c1d6492350049d09e37e127cb8
Eric Tam <ehtcanucks@yahoo.ca>
2012-07-13 16:26:00 -0400

    Adding comments to Game.cpp

--- proj2/Game.cpp
+++ proj2/Game.cpp
@@ -10,20 +10,6 @@ const int MAX_SCORE = 80;
 Game::Game() : currentPlayer_(0), currentTurn_(0) {
     table_ = Table();
     std::string strategy = "";
-    /*for (int i = 1; i <= NUM_PLAYERS; i++) {
-    //create 4 new players
-    std::cout<<"Is player "<<i<<" a human(h) or a computer(c)?\n>";
-    std::cin>>strategy;
-    assert (strategy == "c" || strategy == "C" || strategy == "H" || strategy == "h");
-
-    //table_ will link the new player to the table
-    //strategy determines the type of player; human or computer
-    //deck_ will link the new player to the deck (required only because of the deck command)
-    players_[i-1] = new Player(table_, strategy, deck_);
-
-    //set the players' score to 0 initially
-    playerScores_[i-1] = 0;
-    }*/
 
     for (int i = 1; i <= NUM_PLAYERS; i++) {
         playerScores_[i - 1] = 0;
@@ -67,11 +53,6 @@ void Game::takeTurn () {
 
     //increment the currentTurn_.  It starts at 1, and goes to 52.
     currentTurn_++;
-
-
-
-    //increment currentPlayer_ and loop back to 1st player.
-
 }
 
 //method to determine the player holding the SEVEN SPADES card, who will go first
@@ -81,21 +62,25 @@ void Game::determineFirstPlayer () {
 }
 
 
-//int player parameter passed in from 0 to 3
+// Returns the score of the current player. Note that player is from 0 to 3.
 int Game::getScore(int player) const {
     return playerScores_[player];
 }
 
+// Returns a pointer to an array of player scores
 int const* Game::getScores() const {
     return playerScores_;
 }
 
+// Returns the number of discards for the player for the round.
+// Note that player is from 0 to 3.
 int Game::getDiscard (int player) {
     return getDiscarded(player).size();
 }
 
-//int player parameter passed in from 0 to 3
+// Returns all discarded cards for a player. Note that player is from 0 to 3.
 std::vector<Card> Game::getDiscarded(int player) {
+    // Only get the Cards if the player is not NULL
     if (players_[player])
         return players_[player]->getDiscarded();
 
@@ -103,7 +88,8 @@ std::vector<Card> Game::getDiscarded(int player) {
     return emptyVector; 
 }
 
-//int player parameter passed in from 0 to 3
+// Returns the cards in a player's hand, if player has been initialized.
+// Note that player is from 0 to 3.
 std::vector<Card> Game::getHand(int player) {
     if (players_[player])
         return players_[player]->getHand();
@@ -112,36 +98,47 @@ std::vector<Card> Game::getHand(int player) {
     return emptyVector; 
 }
 
+// Returns a pointer to an array of booleans, signifying which cards have been played.
 bool const* Game::getTable() const {
     return table_.getTable();
 }
 
-//Returns player 1 to 4
+// Returns the current player whose turn it is, from 1 to 4, or 0 if no game is running.
 int Game::getCurrentPlayer() const {
     return currentPlayer_;
 }
 
+// Checks if the current player is a human or computer player.
 bool Game::getCurrentPlayerType() const {
     return players_[currentPlayer_ - 1]->getIsHuman();
 }
 
+// Ends the current game.
 void Game::endGame() {
+    // Sets the number of turns and the current player to zero.
     currentTurn_ = 0;
     currentPlayer_ = 0;
+    // Clears the table of cards played.
     table_.clearTable();
+    // Sets all four player's scores to zero and resets their hand
     for (int i = 0; i < 4; i++) {
         playerScores_[i] = 0;
         if (players_[i])
             players_[i]->clearPlayer();
     }
+
     state_ = QUITGAME;
+
     notify();
 }
 
+// Returns the current game state.
 GameState Game::getCurrentState() const {
     return state_;
 }
 
+// Create and set each player's strategy, along with the random seed, before the deck
+// is shuffled.
 void Game::setPlayers(std::string playerTypes[], int seed) {
     for (int i = 1; i <= NUM_PLAYERS; i++) {
         //table_ will link the new player to the table
@@ -153,6 +150,7 @@ void Game::setPlayers(std::string playerTypes[], int seed) {
         playerScores_[i-1] = 0;
     }
 
+    // Set the seed.
     srand48(seed);
 }
 
@@ -190,7 +188,9 @@ void Game::checkForRoundComplete() {
     }
 }
 
+// Starts the game.
 void Game::startGame() {
+    // Set the current turn number to one and shuffle the deck.
     currentTurn_ = 1;
     deck_.shuffle();
 
@@ -209,48 +209,63 @@ void Game::startGame() {
     //figure out the first player and set currentPlayer_.
     determineFirstPlayer();
 
-    autoTurn();
-    state_ = TAKETURN;
-    checkForRoundComplete();
+    autoTurn();                 // Wait for computer players to take their turns
+    state_ = TAKETURN;          // Set state for human to take their turn
+    checkForRoundComplete();    // Check to see if the computers have completed the game
 
-    notify();
+    notify();                   // Update view.
 }
 
+// Attempts to play a card
 void Game::tryPlayingCard(int cardClicked) {
+    // Do not allow other game states to play a card
     if (getCurrentState() != TAKETURN) {
         return;
     }
 
+    // Grab the card in the hand that was selected.
     std::vector<Card> hand = players_[currentPlayer_ - 1]->getHand();
     Card c = hand[cardClicked];
 
+    // If the card can be played, move to the next player and increment
+    // the turn count.
     if (players_[currentPlayer_ - 1]->playCard(c)) {
         currentPlayer_++;
         if (currentPlayer_ == NUM_PLAYERS + 1) 
             currentPlayer_ = 1;
         currentTurn_++;
     }
+    // If the card is invalid, return.
     else {
         return;
     }
 
+    // Wait for computer players and check for completed rounds due to them
     autoTurn();
     state_ = TAKETURN;
     checkForRoundComplete();
 
-    notify();
+    notify();   // Update view.
 }
 
+// Attempt to discard a card
 void Game::tryDiscardingCard(int cardClicked) {
+    // Only allow humans to discard a card, during a valid game state.
     if (getCurrentState() != TAKETURN) {
         return;
     }
 
+    // Grab the selected card from the hand
     std::vector<Card> hand = players_[currentPlayer_ - 1]->getHand();
     Card c = hand[cardClicked];
 
+    // If the card can be discarded...
     if (players_[currentPlayer_ - 1]->discardCard(c)) {
+        // Add the rank of the card to the player's score
+        // getRank() would return 0 for ACE etc.
         playerScores_[currentPlayer_ - 1] += (int)c.getRank() + 1;
+        
+        // Switch to the next player and turn.
         currentPlayer_++;
         if (currentPlayer_ == NUM_PLAYERS + 1) 
             currentPlayer_ = 1;
@@ -260,22 +275,27 @@ void Game::tryDiscardingCard(int cardClicked) {
         return;
     }
 
+    // Wait for computer players
     autoTurn();
     checkForRoundComplete();
 
-    notify();
+    notify();   // Update view.
 }
 
+// Rage quits
 void Game::rageQuit( int player) {
+    // Change the strategy to computer for the player
     players_[player]->rageQuit();
 
+    // Let computers take their turn
     autoTurn();
     state_ = TAKETURN;
     checkForRoundComplete();
 
-    notify();
+    notify();   // Update view
 }
 
+// Returns the current turn number
 int Game::getCurrentTurn() const {
     return currentTurn_;
 }
